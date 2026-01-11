eb4d9d3b2245db6d8bf811a0bb40c996828c3e53
Saurabh Shirsat <saurabhsachin2003@gmail.com>
2026-01-11 13:54:34 +0530

    Optimize routing with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import Dropshipping from "./pages/Dropshipping";
+import { lazy, Suspense } from "react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-// Pages
-import Home from "./pages/Home";
-import Pricing from "./pages/Pricing";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Blog from "./pages/Blog";
+// Lazy-loaded pages (performance optimized)
+const Home = lazy(() => import("./pages/Home"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Blog = lazy(() => import("./pages/Blog"));
+const Dropshipping = lazy(() => import("./pages/Dropshipping"));
 
 // Amazon pages
-import AmazonIndia from "./pages/AmazonIndia";
-import AmazonInternational from "./pages/AmazonInternational";
+const AmazonIndia = lazy(() => import("./pages/AmazonIndia"));
+const AmazonInternational = lazy(() => import("./pages/AmazonInternational"));
 
 // Service pages
-import Flipkart from "./pages/Flipkart";
-import Meesho from "./pages/meesho";
-import EcomWebsite from "./pages/ecom-website";
-
+const Flipkart = lazy(() => import("./pages/Flipkart"));
+const Meesho = lazy(() => import("./pages/meesho"));
+const EcomWebsite = lazy(() => import("./pages/ecom-website"));
 
 import "./styles/global.css";
 
@@ -28,27 +28,49 @@ function App() {
     <>
       <Header />
 
-      <Routes>
-        {/* Main pages */}
-        <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} />
-<Route path="/dropshipping" element={<Dropshipping />} />
+      {/* Suspense wrapper for lazy loading */}
+      <Suspense
+        fallback={
+          <div
+            style={{
+              minHeight: "60vh",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              background: "#0b1c2d",
+              color: "#fff",
+              fontSize: "18px",
+            }}
+          >
+            Loading...
+          </div>
+        }
+      >
+        <Routes>
+          {/* Main pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/dropshipping" element={<Dropshipping />} />
 
-        {/* Amazon */}
-        <Route path="/amazon-india" element={<AmazonIndia />} />
-        <Route path="/amazon-international" element={<AmazonInternational />} />
+          {/* Amazon */}
+          <Route path="/amazon-india" element={<AmazonIndia />} />
+          <Route
+            path="/amazon-international"
+            element={<AmazonInternational />}
+          />
 
-        {/* Services */}
-        <Route path="/flipkart" element={<Flipkart />} />
-        <Route path="/meesho" element={<Meesho />} />
-        <Route path="/ecom-website" element={<EcomWebsite />} />
+          {/* Services */}
+          <Route path="/flipkart" element={<Flipkart />} />
+          <Route path="/meesho" element={<Meesho />} />
+          <Route path="/ecom-website" element={<EcomWebsite />} />
 
-        {/* Fallback */}
-        <Route path="*" element={<Home />} />
-      </Routes>
+          {/* Fallback */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
 
       <Footer />
     </>
